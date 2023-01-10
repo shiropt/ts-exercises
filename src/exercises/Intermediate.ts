36630bf597aa50b192cddc19bac91afb1630d90f
shiropt <shiro19880610@yahoo.co.jp>
2023-01-10 22:07:49 +0900

    ts-exercises intermediate 3-2

--- src/exercises/Intermediate.ts
+++ src/exercises/Intermediate.ts
@@ -40,3 +40,92 @@ type T2 = MyPartial<{
     piyo: number;
   };
 }>;
+
+/** 3-3. イベント */
+interface EventPayloads {
+  start: {
+    user: string;
+  };
+  stop: {
+    user: string;
+    after: number;
+  };
+  end: {};
+}
+
+class EventDischarger<E> {
+  emit<Ev extends keyof E>(eventName: Ev, payload: E[Ev]) {
+    // 省略
+  }
+}
+
+// 使用例
+const ed = new EventDischarger<EventPayloads>();
+
+ed.emit("start", {
+  user: "user1",
+});
+ed.emit("stop", {
+  user: "user1",
+  after: 3,
+});
+ed.emit("end", {});
+
+// エラー例
+// ed.emit("start", {
+//   user: "user2",
+//   after: 0,
+// });
+// ed.emit("stop", {
+//   user: "user2",
+// });
+// ed.emit("foobar", {
+//   foo: 123,
+// });
+
+/** 3-4. reducer */
+type Action =
+  | {
+      type: "increment";
+      amount: number;
+    }
+  | {
+      type: "decrement";
+      amount: number;
+    }
+  | {
+      type: "reset";
+      value: number;
+    };
+const reducer = (state: number, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return state + action.amount;
+    case "decrement":
+      return state - action.amount;
+    case "reset":
+      return action.value;
+  }
+};
+
+// 使用例
+reducer(100, {
+  type: "increment",
+  amount: 10,
+}) === 110;
+
+reducer(100, {
+  type: "decrement",
+  amount: 55,
+}) === 45;
+
+reducer(500, {
+  type: "reset",
+  value: 0,
+}) === 0;
+
+// エラー例
+// reducer(0, {
+//   type: "increment",
+//   value: 100,
+// });
